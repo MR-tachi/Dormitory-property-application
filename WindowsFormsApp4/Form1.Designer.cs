caa6be20a3ee674c1e8baf7d4c537e74bf997f33
MR-tachi <mr.hajibaba77@gmail.com>
2023-10-15 03:38:57 +0330

    complete

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp4
 {
-    partial class Form1
+    partial class manage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(manage));
+            this.Sabt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Sabt
+            // 
+            this.Sabt.AccessibleName = "Sabt";
+            this.Sabt.Font = new System.Drawing.Font("B Ziba", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Sabt.Location = new System.Drawing.Point(110, 154);
+            this.Sabt.Name = "Sabt";
+            this.Sabt.Size = new System.Drawing.Size(148, 55);
+            this.Sabt.TabIndex = 0;
+            this.Sabt.Text = "ثبت آسیب";
+            this.Sabt.UseVisualStyleBackColor = true;
+            this.Sabt.Click += new System.EventHandler(this.Sabt_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.comboBox1.ForeColor = System.Drawing.Color.DeepPink;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(296, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.comboBox1.Size = new System.Drawing.Size(149, 38);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "آسیب زننده ";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("B Nazanin", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(238, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "شغل آسیب زننده را انتخاب کنید";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("B Nazanin", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(27, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "کد مال";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("B Nazanin", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(25, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "کد ملی";
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "hazf";
+            this.button1.Font = new System.Drawing.Font("B Ziba", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.Location = new System.Drawing.Point(296, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 55);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "حذف آسیب";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "hazf";
+            this.button2.Font = new System.Drawing.Font("B Ziba", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button2.Location = new System.Drawing.Point(110, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(335, 55);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "لیست آسیب زنندگان";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // manage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(514, 351);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sabt);
+            this.DoubleBuffered = true;
+            this.Name = "manage";
+            this.Text = "         ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Sabt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -6,15 +6,163 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp4
 {
-    public partial class Form1 : Form
+    public partial class manage : Form
     {
-        public Form1()
+         SqlConnection Mycon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'C:\Users\Acer\Documents\Visual Studio 2017\Projects\Database1\amval_khabgah.mdf'; Integrated Security = True; Connect Timeout = 30");
+        public manage()
         {
             InitializeComponent();
+            comboBox1.Items.Add(" دانشجو ");
+            comboBox1.Items.Add(" کارمند ");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Sabt_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" | textBox2.Text == "")
+            {
+                MessageBox.Show("کد ملی و کد مال وارد نشده", "اخطار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if(comboBox1.Text=="")
+                MessageBox.Show("نوع آسیب زننده انتخاب نشده!", "اخطار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+
+                string rquery = "insert into rhurt(fk_pcode,fk_ridnumber) values ('" + textBox1.Text + "','" + textBox2.Text + "')";
+                string squery = "insert into shurt(fk_pcode,fk_sidnumber) values ('" + textBox1.Text + "','" + textBox2.Text + "')";
+
+                try
+                {
+
+                    if (comboBox1.Text == " دانشجو ")
+                    {
+                        SqlCommand Mycmd = new SqlCommand(squery, Mycon);
+                        Mycon.Open();
+                        Mycmd.CommandType = CommandType.Text;
+                        Mycmd.ExecuteNonQuery();
+                        MessageBox.Show("ثبت با موفقیت انجام شد", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Mycon.Close();
+                    }
+                    else if (comboBox1.Text == " کارمند ")
+                    {
+                        SqlCommand Mycmd = new SqlCommand(rquery, Mycon);
+                        Mycon.Open();
+                        Mycmd.CommandType = CommandType.Text;
+                        Mycmd.ExecuteNonQuery();
+                        MessageBox.Show("ثبت با موفقیت انجام شد", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Mycon.Close();
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("نوع آسیب زننده یا کد مال یا کد ملی اشتباه است", "اخطار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Mycon.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" | textBox2.Text == "")
+            {
+                MessageBox.Show("کد ملی و کد مال وارد نشده", "اخطار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (comboBox1.Text == "")
+                MessageBox.Show("نوع آسیب زننده انتخاب نشده!", "اخطار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+
+                try
+                {
+                    string rquery = "delete from rhurt where'" + textBox1.Text + "' = fk_pcode and '" + textBox2.Text + "' = fk_ridnumber";
+                    string squery = "delete from shurt where'" + textBox1.Text + "' = fk_pcode and '" + textBox2.Text + "' = fk_sidnumber";
+                    if (comboBox1.Text == " دانشجو ")
+                    {
+                        SqlCommand Mycmd = new SqlCommand(squery, Mycon);
+                        Mycon.Open();
+                        Mycmd.CommandType = CommandType.Text;
+                        Mycmd.ExecuteNonQuery();
+                        MessageBox.Show("حذف با موفقیت انجام شد", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Mycon.Close();
+                    }
+                    else if (comboBox1.Text == " کارمند ")
+                    {
+                        SqlCommand Mycmd = new SqlCommand(rquery, Mycon);
+                        Mycon.Open();
+                        Mycmd.CommandType = CommandType.Text;
+                        Mycmd.ExecuteNonQuery();
+                        MessageBox.Show("حذف با موفقیت انجام شد", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Mycon.Close();
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("نوع آسیب زننده یا کد مال \n یا کد ملی اشتباه است \nو یا آسیب زننده وجود ندارد", "اخطار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Mycon.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "select fname,lname,id_number,fk_pcode , pname,cost " +
+                "from(((hurts join people on national_id = id_number)" +
+                "join dormproperty on hurts.pcode = dormproperty.pcode)" +
+                "join eternalproperty on fk_pcode = hurts.pcode )";
+            SqlCommand Mycmd = new SqlCommand(query, Mycon);
+            SqlDataReader Myread;
+            try
+            {
+                string list = "";
+                Mycon.Open();
+                list += "خسارت      اسم مال       کد مال       کد ملی      نام خانوادگی     نام \n";
+                Myread = Mycmd.ExecuteReader();
+                while (Myread.Read())
+                {
+                    list += Myread["fname"].ToString();
+                    list += "     ";
+                    list += Myread["lname"].ToString();
+                    list += "     ";
+                    list += Myread["id_number"].ToString();
+                    list += "     ";
+                    list += Myread["fk_pcode"].ToString();
+                    list += "     ";
+                    list += Myread["pname"].ToString();
+                    list += "     ";
+                    list += Myread["cost"].ToString();
+                    list += "\n";
+
+                }
+
+                MessageBox.Show(list , "لیست", MessageBoxButtons.OK, MessageBoxIcon.None);
+
+
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+            finally
+            {
+                if (Mycon.State == ConnectionState.Open)
+                    Mycon.Close();
+            }
         }
     }
 }
